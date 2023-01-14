723372d24b8ff8ca0f594026b1985d8c312120b7
DavideSirico <davide.sirico@gmail.com>
2023-01-14 10:37:13 +0100

    lettura di un file csv

--- Java/NetBeans/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Java/NetBeans/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -2,8 +2,26 @@ package javaapplication1;
 
 public class JavaApplication1
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Rettangolo r = new Rettangolo(10,20); 
         System.out.println("Hello World!");
     }
-    
 }
+
+class Rettangolo
+{
+    private int base;
+    private int altezza;
+    Rettangolo(int base, int altezza)
+    {
+        this.base = base;
+        this.altezza = altezza;
+    }
+    int perimetro()
+    {
+        return (base + altezza) * 2;
+    }
+}
+
+class Bagai
