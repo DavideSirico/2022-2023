9100302b79f13d5557cab55c670aa0803b5f007e
DavideSirico <davide.sirico@gmail.com>
2023-03-14 17:53:33 +0100

    .

---
+++ Sito_Stack_Heap/JS/script.js
@@ -0,0 +1,12 @@
+function changeTheme()
+{
+    var theme = document.getElementById("theme");
+    if (theme.getAttribute("href") == "css/light.css")
+    {
+        theme.href = "css/dark.css";
+    }
+    else
+    {
+        theme.href = "css/light.css";
+    }
+}
