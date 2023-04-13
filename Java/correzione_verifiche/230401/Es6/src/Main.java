1eef01873a3a4b854d0cbd6bb1db14d39bfb8498
DavideSirico <davide.sirico@gmail.com>
2023-04-13 20:49:40 +0200

    .

--- Java/correzione_verifiche/230401/Es6/src/Main.java
+++ Java/correzione_verifiche/230401/Es6/src/Main.java
@@ -7,7 +7,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		try {
-			FileReader fileReader = new FileReader("src/numeri.txt");
+			FileReader fileReader = new FileReader("numeri.txt");
 			BufferedReader input = new BufferedReader(fileReader);
 			
 			String currentNumber;
@@ -42,41 +42,99 @@ public class Main
 		System.out.println("C1" + C);
 	}
 	
-
+	// non funziona
 	public static void elimina(Queue<Integer> C)
 	{
-		Queue<Integer> temp = new LinkedList<>();
+		Queue<Integer> temp1 = new LinkedList<>();
+		Queue<Integer> temp2 = new LinkedList<>();
+		Queue<Integer> temp3 = new LinkedList<>();
+		Queue<Integer> ris = new LinkedList<>();
+
+		C = reverse(C);
 		while(!C.isEmpty())
 		{
 			int current = C.remove();
-			temp.add(current);
+			temp1.add(current);
+			temp2.add(current);
 		}
-		while(!temp.isEmpty())
+		temp1 = reverse(temp1);
+
+		while(!temp1.isEmpty())
 		{
-			int current = temp.remove();
-			if(!isDuplicate(C, current))
+			int top = myPeek(temp1);
+			boolean duplicato = false;
+			int current = temp1.remove();
+			while(!temp2.isEmpty())
 			{
-				C.add(current);
+				temp2.remove();
+			}
+			temp3 = copy(temp2);
+			while(!temp2.isEmpty())
+			{
+				if(top==current)
+				{
+					duplicato = true;
+					temp1.remove();
+				}
+				current = temp2.remove();
+			}
+			if(!duplicato)
+			{
+				ris.add(current);
 			}
 		}
+		System.out.println("temp1: "+temp1);
+		System.out.println("temp2: "+temp2);
+		System.out.println("temp3: "+temp3);
+		System.out.println("ris: "+ris);
 	}
-	public static boolean isDuplicate(Queue<Integer> C, int n)
+	public static int myPeek(Queue<Integer> C)
 	{
 		Queue<Integer> temp = new LinkedList<>();
-		boolean duplicate = false;
+
+		int top = C.remove();
+		temp.add(top);
 		while(!C.isEmpty())
 		{
-			int current = C.remove();
-			if(current==n)
-			{
-				duplicate = true;
-			}
-			temp.add(current);
+			temp.add(C.remove());
 		}
+
 		while(!temp.isEmpty())
 		{
 			C.add(temp.remove());
 		}
-		return duplicate;
+
+		return top;
+
+	}
+	public static Queue<Integer> reverse(Queue<Integer> Q)
+	{
+		if(Q.isEmpty())
+		{
+			return Q;
+		} else{
+			int current = Q.remove();
+			Q = reverse(Q);
+			Q.add(current);
+			return Q;
+		}
+	}
+	public static Queue<Integer> copy(Queue<Integer> Q)
+	{
+		Queue<Integer> temp1 = new LinkedList<>();
+		Queue<Integer> temp2 = new LinkedList<>();
+		while(!Q.isEmpty())
+		{
+			int current = Q.remove();
+			temp1.add(current);
+			temp2.add(current);
+		}
+
+		while(!temp2.isEmpty())
+		{
+			Q.add(temp2.remove());
+		}
+		return temp1;
+
 	}
 }
